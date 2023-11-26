c881f13f413cf552627f053df23fbcd44627adfa
Tanvir Ramim <ramimhossan14@gmail.com>
2023-11-26 16:41:41 +0600

    add camps api added

--- index.js
+++ index.js
@@ -29,12 +29,32 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
        const usersCollection=client.db('MediCampsHub').collection('users')
+       const campsCollection=client.db('MediCampsHub').collection('camps')
       
+        
+      //  camps related api
+
+      app.post('/camps',async (req,res)=>{
+           try{
+            const campsInfo=req.body
+           console.log(campsInfo)
+           const result=await campsCollection.insertOne(campsInfo)
+           return res.send(result)
+           }
+           catch{
+            return res.send(result)
+           }
+      })
+
+
+
+
+
 
       //  users related Api
-
      app.get('/userRole/:email',async(req,res)=>{
-              const email=req.params.email
+              try{
+                const email=req.params.email
                const query={email:email}
                const result =await usersCollection.findOne(query,{
                 projection:{
@@ -43,18 +63,40 @@ async function run() {
                   _id: 0
                 }
                })
-               res.send(result)
+             return   res.send(result)
+              }
+              catch{
+                return res.send({error:true})
+              }
      })
-
+     
+     app.get('/hp',async(req,res)=>{
+             try{
+              const role='Healthcare Professional'
+              const query={role:role}
+              const result =await usersCollection.find(query
+              ).toArray()
+              res.send(result)
+             }
+             catch{
+              return res.send({error:true})
+             }
+     })
+    
      app.post('/users',async(req,res)=>{
-        const user=req.body 
+        try{
+          const user=req.body 
         const query={email:user.email}
         const existingUser=await usersCollection.findOne(query)
         if(existingUser){
            return res.send({message: 'user already exists', insertedId: null})
         }
         const result=await usersCollection.insertOne(user)
-        res.send(result)
+       return  res.send(result)
+        }
+        catch{
+          return res.send({error:true})
+        }
      })
 
 
